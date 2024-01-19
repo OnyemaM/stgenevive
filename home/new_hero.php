5fb0b960978f3449e795ed457cdd29d3447b58ae
OnyemaM <onyema.mbadiwe@stghcs.com>
2024-01-19 15:11:05 +0100

    2nd Commit 19 01 24

---
+++ home/new_hero.php
@@ -0,0 +1,18 @@
+<div class="core_segment_one" style="background-image:url('https://lavenduct.com/stghs/assets/image_central_one.png')">
+    
+    <div class="segment_container d-lg-flex align-items-center justify-content-center">
+        <div class="core_segment_one_inner d-md-flex align-items-center justify-content-center">
+        <div class="row">
+        <div class="col-md-12 cta_area">
+            <img class="index_image" src="https://lavenduct.com/stghs/assets/index_image_1a.png">
+            <div class="core_title_one">Experience <br>Real Healthcare</div>
+            <div class="core_sub_title_one">Discover unparalleled care with St. Genevieve. Our dedicated companions provide essential support, including housekeeping, laundry, meal preparation, and overall assistance with health and wellness.</div>
+            <button class="learn_more_inverted_btn mt-3">Learn More<i class="fa-solid fa-angle-right"></i></button>
+        </div>
+    </div>
+    </div>
+    <div class="core_segment_one_inner"></div>
+    </div>
+    
+    
+    </div>
